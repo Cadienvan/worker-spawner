2fbc64c683a3ae8a20f3f24231a1d9dabf8589a7
Michael Di Prisco <cadienvan@gmail.com>
2022-12-30 11:20:32 +0100

    feat: added additional options

---
+++ src/defaults.ts
@@ -0,0 +1,4 @@
+export const defaultOptions = {
+  unref: false,
+  timeout: 0,
+}
--- src/lib.ts
+++ src/lib.ts
@@ -1,10 +1,17 @@
 import { Worker } from 'worker_threads';
+import { defaultOptions } from './defaults';
+import { Options } from './models';
 
 export function workerThreadExecute<T>(
   workerPathOrInstance: string | Worker,
-  workerData: unknown
+  workerData: unknown = undefined,
+  _options: Partial<Options> = {}
 ): Promise<T> {
   return new Promise((resolve, reject) => {
+    const options: Options = {
+      ...defaultOptions,
+      ..._options
+    };
     let worker: string | Worker;
     if (typeof workerPathOrInstance === 'string') {
       worker = new Worker(workerPathOrInstance, {
@@ -14,11 +21,20 @@ export function workerThreadExecute<T>(
       worker = workerPathOrInstance;
       worker.postMessage(workerData);
     }
+    if (options.unref) {
+      worker.unref();
+    }
     worker.on('message', resolve);
     worker.on('error', reject);
     worker.on('exit', (code) => {
       if (code !== 0)
         reject(new Error(`Worker stopped with exit code ${code}`));
     });
+    if (options.timeout > 0) {
+      setTimeout(() => {
+        (worker as Worker).terminate();
+        reject(new Error('Worker timed out'));
+      }, options.timeout).unref();
+    }
   });
 }
